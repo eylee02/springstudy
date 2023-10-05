05449b9c4020b2069604ec8ddffa9519134fdd6e
eylee02 <leywindy@naver.com>
2023-10-05 12:21:25 +0900

    20231005

--- 05_DI/src/main/java/com/gdu/app05/dao/BoardDao.java
+++ 05_DI/src/main/java/com/gdu/app05/dao/BoardDao.java
@@ -3,9 +3,12 @@ package com.gdu.app05.dao;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.gdu.app05.dto.BoardDto;
 
+@Repository  // DAO 전용 @Component, BoardDao 타입의 boardDao 객체를 Spring container에 생성해 둔다.
 public class BoardDao {
   
   public List<BoardDto> getBoardList(){
--- 05_DI/src/main/java/com/gdu/app05/service/BoardServiceImpl.java
+++ 05_DI/src/main/java/com/gdu/app05/service/BoardServiceImpl.java
@@ -3,37 +3,21 @@ package com.gdu.app05.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.gdu.app05.dao.BoardDao;
 import com.gdu.app05.dto.BoardDto;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor  // final field 전용 생성자,
+                          // @Autowired를 이용한 생성자 주입을 위해서 추가한다.
+@Service  // 서비스 계층(Business Layer) 전용 @Component, Spring container에 BoardService boardServiceImpl 객체를 생성해 둔다.
 public class BoardServiceImpl implements BoardService {
-  
-  /*
-   * BoardService DI 처리 방법
-   * 
-   * 1. BoardDao 타입의 BoardDao 객체를 Spring Container에 넣는다. (아래 3가지 방법 중 하나 이용)
-   *  1) <bean> 태그            : /WEB-INF/spring/root-context.xml
-   *  2) @Configuration + @Bean : com.gdu.app05.config.AppConfig.java
-   *  3) @Component    
-   *          
-   * 2. @Autowired를 이용해서 Spring Container에서 BoardDao 타입의 객체를 가져온다.
-   *  1) 필드에 주입하기
-   *  2) 생성자에 주입하기
-   *  3) Setter 형식의 메소드에 주입하기
-   */
-  
-  @Autowired
-  private BoardDao boardDao;
-
-
-  public BoardDao getBoardDao() {
-    return boardDao;
-  }
-
-  public void setBoardDao(BoardDao boardDao) {
-    this.boardDao = boardDao;
-  }
+ 
+  // 주입된 boardDao 객체의 변경 방지를 위해 final 처리한다.
+  private final BoardDao boardDao;
 
   @Override
   public List<BoardDto> getBoardList() {
