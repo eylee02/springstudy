ad4e99f20012a069c189de9afb1aa0d9d36129f5
eylee02 <leywindy@naver.com>
2023-10-16 20:04:08 +0900

    20231016

---
+++ notice_01/src/main/java/com/gdu/app/dao/NoticeMapper.java
@@ -0,0 +1,14 @@
+package com.gdu.app.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gdu.app.dto.NoticeDto;
+
+@Mapper
+public interface NoticeMapper {
+  
+  public int addNotice(NoticeDto noticeDto);
+  public List<NoticeDto> getNoticeList();
+}
